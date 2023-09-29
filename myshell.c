45383424a906d4f9bdcf6af136f8ad2fa5405e8e
Auwate <111798627+Auwate@users.noreply.github.com>
2023-09-29 17:41:23 -0400

    Update myshell.c
    Added new functionality:
    
    stop [processID]
    kill [processID]
    continue [processID]
    quit
    run [command]
    
    Example:
    myshell> start ls
    myshell: process 150 started
    myshell> stop 150
    myshell: process 150 stopped
    myshell> continue 150
    myshell: process 150 continued
    myshell> kill 150
    myshell: process 150 killed
    myshell> run ls
    myshell: process 150 exited normally with status 0 
    myshell> quit

--- myshell.c
+++ myshell.c
@@ -5,12 +5,13 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <errno.h>
+#include <signal.h>
 
 #define STRING_LENGTH 255
 #define DELIMINATORS " \t\n"
 
-void tokenizeString (char * line, char * deliminators, char ** tokenizedString[]);
-void findCommand (char ** command);
+int tokenizeString (char * line, char * deliminators, char ** tokenizedString[]);
+void findCommand (char ** command, int amountOfWords);
 void errorChecker ();
 
 int main (void) {
@@ -30,12 +31,13 @@ int main (void) {
     // Get user input
     if (fgets(userCommand, STRING_LENGTH ,stdin) == NULL) {
       fprintf(stderr, "ERROR READING INPUT\n");
-      
+    }
+    if (strcmp(userCommand, "\n") == 0) {
+      continue;
     }
     // Tokenize
-    tokenizeString(userCommand, DELIMINATORS, tokenizedString);
-
-    findCommand((*tokenizedString));
+    int amountOfWords = tokenizeString(userCommand, DELIMINATORS, tokenizedString);
+    findCommand(*tokenizedString, amountOfWords);
     
   }
 
@@ -46,7 +48,18 @@ Logic: errorChecker() takes no parameter, as it uses the errno from the errno.h 
 */
 void errorChecker () {
 
-  if (errno != 0) {
+  if (errno == 0) {
+    errno = 0;
+    return;
+  }
+
+  if (errno == 3) {
+    fprintf(stdout, "myshell: an error occurred: No such process\n");
+    errno = 0;
+    return;
+  }
+
+  else {
 
     printf("myshell: an error occurred: %s\n", strerror(errno));
     exit(1);
@@ -60,7 +73,7 @@ Parameters:
 char * command: First word in the char ** tokenizedString[] array. In other words, it is the command the user wants to execute
 Logic: Using strcmp(...), this method will find the command the user wanted to execute, and will print "unknown command" if it cannot find it.
 */
-void findCommand (char * command[]) {
+void findCommand (char * command[], int amountOfWords) {
 
   if (strcmp(command[0], "wait") == 0) {
     int * status = (int *) malloc (sizeof(int));
@@ -73,23 +86,28 @@ void findCommand (char * command[]) {
         fprintf(stdout, "myshell: no processes left\n");
       }
       else {
-        fprintf(stdout, "process %d exited abnormally with signal %d: %s\n", processID, *status, strerror(errno));
+        fprintf(stdout, "myshell: process %d exited abnormally with signal %d: %s\n", processID, *status, strerror(errno));
       }
       
     }
 
     else {
 
-      fprintf(stdout, "process %d exited normally with status %d\n", processID, *status);
+      fprintf(stdout, "myshell: process %d exited normally with status %d\n", processID, *status);
       
     }
 
-    // Implement processID == 0 and
-    // processID > 1
+    return;
     
   }
 
-  else if (strcmp(command[0], "start") == 0) {
+  else if (strcmp(command[0], "exit") == 0 || strcmp(command[0], "quit") == 0) {
+    
+    exit(1);
+    
+  }
+
+  else if (strcmp(command[0], "start" ) == 0 && amountOfWords > 1) {
 
     pid_t process = fork();
 
@@ -99,7 +117,7 @@ void findCommand (char * command[]) {
       errorChecker();
     }
     else if (process > 0) {
-      fprintf(stdout, "process %d started\n", process);
+      fprintf(stdout, "myshell: process %d started\n", process);
       return;
     }
     else {
@@ -108,15 +126,72 @@ void findCommand (char * command[]) {
     
   }
 
-  else if (strcmp(command[0], "exit") == 0) {
+  else if (strcmp(command[0], "kill") == 0 && amountOfWords > 1) {
+
+    int errorOccurred = kill(atoi(command[1]), SIGKILL);
+
+    if (errorOccurred == -1) {
+      errorChecker();
+    }
+
+    else {
+      printf("myshell: process %s killed\n", command[1]);
+    }
     
-    exit(1);
+  }
+
+  else if (strcmp(command[0], "stop") == 0 && amountOfWords > 1) {
+
+    int errorOccurred = kill (atoi(command[1]), SIGSTOP);
+
+    if (errorOccurred == -1) {
+      errorChecker();
+    }
+
+    else {
+      printf("myshell: process %s stopped\n", command[1]);
+    }
+    
+  }
+
+  else if (strcmp(command[0], "continue") == 0 && amountOfWords > 1) {
+
+    int errorOccurred = kill (atoi(command[1]), SIGCONT);
+
+    if (errorOccurred == -1) {
+      errorChecker();
+    }
+
+    else {
+      printf("myshell: process %s continued\n", command[1]);
+    }
+      
+  }
+
+  else if (strcmp(command[0], "run") == 0 && amountOfWords > 1) {
+
+    int * status = (int *) malloc (sizeof(int));
+    pid_t process = fork();
+
+    if (process == 0) {
+      execvp(command[1], (command)+1);
+    }
+
+    pid_t processID = waitpid(process, status, WUNTRACED);
+
+    if (processID == -1) {
+      fprintf(stdout, "myshell: process %d exited abnormally with signal %d: %s\n", processID, *status, strerror(errno));
+    }
+
+    else {
+      fprintf(stdout, "myshell: process %d exited normally with status %d\n", processID, *status);
+    }
     
   }
 
   else {
     
-    printf("unknown command: %s\n", command[0]);
+    printf("myshell: unknown command: %s\n", command[0]);
     
   }
   
@@ -129,7 +204,7 @@ void findCommand (char * command[]) {
   char ** tokenizedString[]: A pointer to a list of strings. The reason why it's a pointer is to edit the memory here in the method and to then read it back in main.
   Logic: Takes a string and splits it into its constitutent parts using the deliminators given.
 */
-void tokenizeString (char * line, char * deliminators, char ** tokenizedString[]) {
+int tokenizeString (char * line, char * deliminators, char ** tokenizedString[]) {
 
   char * token = strtok(line, DELIMINATORS);
 
@@ -139,5 +214,7 @@ void tokenizeString (char * line, char * deliminators, char ** tokenizedString[]
     (*tokenizedString)[i++] = token;
     token = strtok(0, DELIMINATORS);
   }
+
+  return i;
   
 }
